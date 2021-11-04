5688e99fac295818f81d507a7baea5688c63d559
Kent Hiroi <khiroi@mail.sfsu.edu>
2021-11-03 20:24:58 -0700

    finished UI

--- src/index.js
+++ src/index.js
@@ -34,12 +34,40 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("char-body").style.marginLeft = "0";
   })
 
-  
+
   const charselect = document.getElementsByClassName("char-select")[0];
 
   const openCSS = document.getElementById("select-btn"); // opencss = openCharacterSelectScreen
+
+  // const aboutPg = document.getElementById("about"); // opencss = openCharacterSelectScreen
+  const glossaryBtn = document.getElementById("glossary-btn");
+  const glossaryDiv = document.getElementById("glossary");
+  const aboutBtn = document.getElementById("about-btn");
+  const aboutDiv = document.getElementById("about");
+  const homeDiv = document.getElementById("homepage");
+  const charInfoDiv = document.getElementById("char-info");
   openCSS.addEventListener("click", function(){
     charselect.style.display = "block";
+  });
+
+  glossaryBtn.addEventListener("click", function(){
+    homeDiv.style.display = "none";
+    charselect.style.display = "none";
+    glossaryDiv.style.display = "block";
+    aboutDiv.style.display = "none";
+    charInfoDiv.style.display = "none";
+    document.getElementById("sandwich").style.width = "0";
+    document.getElementById("char-body").style.marginLeft = "0";
+  });
+
+  aboutBtn.addEventListener("click", function(){
+    homeDiv.style.display = "none";
+    charselect.style.display = "none";
+    glossaryDiv.style.display = "none";
+    aboutDiv.style.display = "block";
+    charInfoDiv.style.display = "none";
+    document.getElementById("sandwich").style.width = "0";
+    document.getElementById("char-body").style.marginLeft = "0";
   });
 
   const movemodal = document.getElementsByClassName("move-modal")[0].parentNode
@@ -54,15 +82,19 @@ document.addEventListener("DOMContentLoaded", () => {
       document.getElementById("char-body").style.marginLeft = "0";
     }
     if (charArr.includes(event.target)){ 
+      homeDiv.style.display = "none";
       charselect.style.display = "none";
-        document.getElementById("sandwich").style.width = "0";
-        document.getElementById("char-body").style.marginLeft = "0";
+      glossaryDiv.style.display = "none";
+      aboutDiv.style.display = "none";
+      charInfoDiv.style.display = "block";
+      document.getElementById("sandwich").style.width = "0";
+      document.getElementById("char-body").style.marginLeft = "0";
     }
+    if (event.target.getAttribute(""))
     if (event.target.getAttribute("class") === "cell"){ //if a move line was clicked
       const moveArr = event.target.parentNode.dataset.moveid;
       const charName = event.target.parentNode.dataset.char;
       if (moveArr !== "sticky-row" && moveArr){
-        // console.log(granInfo[moveArr]);
         movemodal.style.display = "block";
         if (charName === 'Gran'){
           modalPopulate(granInfo[moveArr]);
