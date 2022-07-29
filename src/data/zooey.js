74098768c902faf9fb0b63cbfe1ec48d7607919a
Kent Hiroi <khiroi@mail.sfsu.edu>
2022-07-28 21:50:58 -0700

    OD corrections

--- src/data/zooey.js
+++ src/data/zooey.js
@@ -343,6 +343,69 @@ const uOverhead = {
   description: "Situationally can lead to a combo.\nZooey performs a short hop before hitting the opponent on the head with her sword. Has exceptional range for a Universal Overhead. Just like her throws, Zooey can set up Summon Dragon and use this in between lasers for a perfectly safe overhead. She can also use a negative edge Thunder release to make it plus on block or to start a combo on hit."
 };
 
+
+//Tactical moves
+
+const tacticalRush = {
+  moveName: 'Rush (Normal)',
+  damage: '-', 
+  guard: '-', 
+  startup: '-', 
+  active: '-', 
+  recovery: '68 (Entire move)', 
+  onblock: '-', 
+  onhit: '-',
+  oncounterhit: '-',
+  clash: '-',
+  motion: ['6', 'm', 'h'],
+  description: '',
+}
+
+const tacticalRushCancel = {
+  moveName: 'Rush (Cancel)',
+  damage: '-', 
+  guard: '-', 
+  startup: '8', 
+  active: '15', 
+  recovery: '20', 
+  onblock: '+4', 
+  onhit: 'KD (+22)',
+  oncounterhit: 'KD (+22)',
+  clash: '-',
+  motion: ['6', 'm', 'h'],
+  description: 'Strike invulnerable on frame 1-22.',
+}
+
+const tacticalShift = {
+  moveName: 'Back Shift',
+  damage: '-', 
+  guard: '-', 
+  startup: '-', 
+  active: '-', 
+  recovery: '30', 
+  onblock: '-', 
+  onhit: '-',
+  oncounterhit: '-',
+  clash: '-',
+  motion: ['4', 'm', 'h'],
+  description: 'Invincible on frame 1-15.',
+}
+
+const overdriveActivation = {
+  moveName: 'Overdrive',
+  damage: '-', 
+  guard: 'Unblockable', 
+  startup: '26', 
+  active: '3', 
+  recovery: '33', 
+  onblock: '+19', 
+  onhit: 'KD (+19)',
+  oncounterhit: 'KD (+19)',
+  clash: '-',
+  motion: ['m', 'h'],
+  description: 'Invincible on frame 1-28, counterhit state on frame 29-61.'
+}
+
 // fireballs
 const lFireball = { 
   moveName: 'Immortal Thrust', 
